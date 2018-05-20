a1163dded28f1455cb32e9750def3b7f9dceab86
David Dolata <dolatada@gmail.com>
2018-05-19 20:47:26 -0700

    Adds comments"

--- apple_stock.py
+++ apple_stock.py
@@ -12,13 +12,19 @@ url = 'https://assets.datacamp.com/production/course_1639/datasets/messy_stock_d
 
 # Create DataFrame
 # Create DataFrame
+df = pd.read_csv(url, sep = ',', dtype=None, low_memory = False)
+# print(df)
+
+# # Inspect DataFrame
+# print(type(df))
+# print(df.shape)
+# print(df.columns)
+# print(df.index)
+
+# Adjust DataFrame, set index, skip/delete rows, etc...
 df = pd.read_csv(url, sep = ',', skiprows = 3, header=0, dtype=None, low_memory = False)
 df = df.drop(0)
 df = df.drop(3)
 df = df.drop(6)
 df = df.reset_index(drop=True)
 print(df)
-
-# print(df.tail())
-# print(df.info())
-# print(df.describe())
