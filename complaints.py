0172612bd5c79f641038eff021c8525fb60b389c
David Dolata <dolatada@gmail.com>
2018-06-03 15:01:37 -0700

    Adds sub df to focus on Navient LLC

--- complaints.py
+++ complaints.py
@@ -14,7 +14,7 @@ import datetime
 import re
 
 # Read in fakenames.csv file to DataFrame
-file = '/Users/daviddolata/Desktop/Complaints.csv'
+file = '/Users/daviddolata/Coding/Python/python-datacamp-practice/DataSets/Complaints.csv'
 # col_list = ['Number', 'Gender', 'NameSet', 'GivenName', 'Surname', 'City', 'State', 'ZipCode', 'Country', 'EmailAddress', 'TelephoneNumber', 'Birthday']
 df = pd.read_csv(file) 
 date_obj = df['Date received'].astype(str)
@@ -28,4 +28,17 @@ high_complaints = df['Company'].value_counts().rename('high_complaints')
 
 df_high = df.merge(high_complaints.to_frame(), left_on='Company', right_index=True)
 df_high = df_high.loc[df_high['high_complaints']>99, :]
-print(df_high['Company'].value_counts())
+# print(df_high['Sub-product'].value_counts())
+
+# Create student_loan DataFrame to inspect companies 
+student_loan = df_high.loc[df_high['Sub-product']=='Federal student loan', :]
+# print(student_loan.Company.value_counts())
+
+# Create Navient DataFrame
+navient = df_high.loc[df_high['Company']=='Navient Solutions, LLC.', :]
+navient_sub = navient.loc[navient['Sub-product']=='I do not know', :]
+print(navient_sub['Sub-issue'].value_counts())
+
+# No numeric data to plot
+navient_sub.plot()
+plt.show()
